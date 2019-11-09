ba7ce0634e2025c9f994d61eb2c6bbfde28197a3
Allan Girao <allan.girao@gmail.com>
2019-11-09 10:19:04 -0300

    Mysql container for tests.

---
+++ init_db/mysql/init.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS ckan;
+USE ckan;
+
+CREATE TABLE IF NOT EXISTS test_table(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	description VARCHAR(200) NOT NULL
+);
+
+INSERT INTO test_table(description)
+VALUES
+('mysql_desc1'),
+('mysql_desc2'),
+('mysql_desc3'),
+('mysql_desc4'),
+('mysql_desc5'),
+('mysql_desc6');
